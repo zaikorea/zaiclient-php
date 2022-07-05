d0dd76968b233e13a59da57cf62d16317541a2d7
Uiseop Eom <ytrtef@gmail.com>
2022-07-05 04:42:29 +0000

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Z.Ai API Client
+
+A PHP client for easy use of the Z.Ai Collector/Recommender API.
+
+
---
+++ src/ZaiClient/ZaiClient.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Z.Ai API client
+ *  @author Uiseop Eom <aesop@zaikorea.org>
+ */
+
+
+namespace Zaikorea\ZaiClient;
+
+/**
+ * Client for easy usage of Z.Ai recommendation API
+ */
+class Client{
+	protected $zai_client_id;
+	protected $zai_secret;
+}
+
+
+?>
