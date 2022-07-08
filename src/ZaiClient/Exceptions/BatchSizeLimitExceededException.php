dea7b84aca9debe1460ffd1cd9f8bc7794a71965
Uiseop Eom <ytrtef@gmail.com>
2022-07-08 22:36:53 +0900

    feat: BatchSizeLimit Exception

---
+++ src/ZaiClient/Exceptions/BatchSizeLimitExceededException.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * BatchSizeLimitExceededException
+ * @author Uiseop Eom <tech@zaikorea.org>
+ * @modifiedBy <name>
+ */
+
+namespace ZaiKorea\ZaiClient\Exceptions;
+
+use \GuzzleHttp\Exception\BadResponseException;
+
+/**
+ * 
+ */
+class BatchSizeLimitExceededException extends \Exception {
+    public function __construct() {
+        $message = sprintf("Number of total records cannot exceed 50, but your Event holds %d.");
+        parent::__construct($message, 2, $previous);
+    }
+
+    public function getHttpStatusCode() {
+        return $this->http_status_code;
+    }
+
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
