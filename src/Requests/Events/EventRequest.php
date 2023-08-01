a8acddb1f0b9194a47933d0be741c5651c8866c6
Uiseop Eom <ytrtef@gmail.com>
2023-08-01 02:37:48 +0000

    feat: Add EventRequest Class

---
+++ src/Requests/Events/EventRequest.php
@@ -0,0 +1,178 @@
+<?php
+namespace ZaiClient\Requests\Events;
+
+use InvalidArgumentException;
+use ZaiClient\Configs\Config;
+use ZaiClient\Exceptions\BatchSizeLimitExceededException;
+use ZaiClient\Requests\Request;
+
+class EventRequest extends Request
+{
+
+    public $user_id;
+    public $item_ids;
+    public $timestamp;
+    public $event_type;
+    public $event_values;
+    public $from_values;
+    public $is_zai_recommendations;
+    public $_timestamp;
+    public $payload;
+
+    /**
+     * @param string $user_id
+     * @param array[string] $item_ids
+     * @param float $timestamp
+     * @param string $event_type
+     * @param array[string] $event_values
+     * @param array[string] $from_values
+     * @param array[bool] $is_zai_recommendations
+     */
+    function __construct(
+        $user_id,
+        $item_ids,
+        $timestamp,
+        $event_type,
+        $event_values,
+        $from_values,
+        $is_zai_recommendations
+    ) {
+
+        $this->validate(
+            $user_id,
+            $item_ids,
+            $timestamp,
+            $event_type,
+            $event_values,
+            $from_values,
+            $is_zai_recommendations
+        );
+
+
+        $events = [];
+        $tmp_timestamp = $timestamp;
+        $this->_timestamp = $timestamp;
+        parent::__construct("POST", config::COLLECTOR_API_ENDPOINT);
+        // $i = 0;
+        // foreach (array_combine($item_ids, $event_values) as $item_id => $event_value) {
+        //     if ($i < count($from_values)) {
+        //         $from_value = $from_values[$i];
+        //     } else {
+        //         $from_value = null;
+        //     }
+        //     if ($i < count($is_zai_recommendations)) {
+        //         $is_zai_recommendation = $is_zai_recommendations[$i];
+        //     } else {
+        //         $is_zai_recommendation = null;
+        //     }
+        //     array_push($events, json_decode(Event::json(
+        //         $user_id,
+        //         $item_id,
+        //         $tmp_timestamp,
+        //         $event_type,
+        //         substr($event_value, 0, 500),
+        //         substr($from_value, 0, 500),
+        //         $is_zai_recommendation
+        //     ), true));
+        //     $tmp_timestamp += config::EPSILON;
+        //     $i += 1;
+        // }
+        // if (count($events) > config::BATCH_REQUEST_CAP) {
+        //     throw new BatchSizeLimitExceededException();
+        // }
+        // if (count($events) == 0) {
+        //     throw new EmptyBatchException();
+        // }
+        // if (count($events) == 1) {
+        //     $this->payload = $events[0];
+        // } else {
+        //     $this->payload = $events;
+        // }
+    }
+
+    function getTimestamp()
+    {
+        return $this->_timestamp;
+    }
+
+    function getPath($client_id)
+    {
+        return config::EVENTS_API_PATH;
+    }
+
+    function getPayload($is_test = false)
+    {
+        if ($is_test) {
+            if (is_array($this->payload)) {
+                foreach ($this->payload as &$event) {
+                    $event["time_to_live"] = config::TEST_EVENT_TIME_TO_LIVE;
+                }
+            } else {
+                $this->payload["time_to_live"] = config::TEST_EVENT_TIME_TO_LIVE;
+            }
+        }
+        return $this->payload;
+    }
+
+    function getQueryParam()
+    {
+        return [];
+    }
+
+    private function validate(
+        $user_id,
+        $item_ids,
+        $timestamp,
+        $event_type,
+        $event_values,
+        $from_values,
+        $is_zai_recommendations
+    ) {
+        if (!is_string($user_id)) {
+            throw new InvalidArgumentException('user_id must be a string');
+        }
+        if (!is_array($item_ids)) {
+            throw new InvalidArgumentException('item_ids must be an array of strings');
+        } else {
+            foreach ($item_ids as $id) {
+                if (!is_string($id)) {
+                    throw new InvalidArgumentException('Each id in item_ids must be a string');
+                }
+            }
+        }
+        if (!is_float($timestamp)) {
+            throw new InvalidArgumentException('timestamp must be a float');
+        }
+        if (!is_string($event_type)) {
+            throw new InvalidArgumentException('event_type must be a string');
+        }
+        if (!is_array($event_values)) {
+            throw new InvalidArgumentException('event_values must be an array of strings');
+        } else {
+            foreach ($event_values as $value) {
+                if (!is_string($value)) {
+                    throw new InvalidArgumentException('Each value in event_values must be a string');
+                }
+            }
+        }
+        if (!is_array($from_values)) {
+            throw new InvalidArgumentException('from_values must be an array of strings');
+        } else {
+            foreach ($from_values as $value) {
+                if (!is_string($value)) {
+                    throw new InvalidArgumentException('Each value in from_values must be a string');
+                }
+            }
+        }
+        if (!is_array($is_zai_recommendations)) {
+            throw new InvalidArgumentException('is_zai_recommendations must be an array of booleans');
+        } else {
+            foreach ($is_zai_recommendations as $recommendation) {
+                if (!is_bool($recommendation)) {
+                    throw new InvalidArgumentException('Each value in is_zai_recommendations must be a boolean');
+                }
+            }
+        }
+    }
+
+}
